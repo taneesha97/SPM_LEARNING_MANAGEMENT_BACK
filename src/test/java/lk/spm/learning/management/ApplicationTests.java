5fafb5499312fa563d93594548f187206d8a0aef
Nirmith Anawaratne <Nirmith.Anawaratne-Intern@dialog.lk>
2021-07-23 21:56:57 +0530

    Init commitment

---
+++ src/test/java/lk/spm/learning/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.spm.learning.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
