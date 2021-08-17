02e307ec683d2f41da74c38b2fc4a51ae7f59fce
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-17 23:40:57 +0530

    SPM database backend initialized.

---
+++ src/main/java/lk/spm/learning/management/model/Item.java
@@ -0,0 +1,31 @@
+package lk.spm.learning.management.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    private String id;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
