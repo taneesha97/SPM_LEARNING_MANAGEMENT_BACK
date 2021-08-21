e9fe7a7ff7b90dc33ef031e5357b2f443a0b34e9
Taneesha <taneesha.chandrasena@acentura.com>
2021-08-21 17:00:29 +0530

    create repository class

---
+++ src/main/java/lk/spm/learning/management/repository/ClassRepository.java
@@ -0,0 +1,9 @@
+package lk.spm.learning.management.repository;
+
+import lk.spm.learning.management.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRepository extends JpaRepository<Course, Long> {
+}
