ae01903cccfafe57d4b21e8e7449acd38228da86
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-19 21:37:07 +0530

    Creating a method to save the uploaded files to the database.

--- src/main/java/lk/spm/learning/management/controller/FileController.java
+++ src/main/java/lk/spm/learning/management/controller/FileController.java
@@ -9,17 +9,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-@Controller
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class FileController {
 
     private final FileStorageService fileStorageService;
@@ -57,5 +56,10 @@ public class FileController {
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(mimeType))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline;fileName=" + resource.getFilename())
                 .body(resource);
+    }
+
+    // Method to save the uploaded files in the database.
+    public void saveFilesToTheDatabase() {
+
     }
 }
