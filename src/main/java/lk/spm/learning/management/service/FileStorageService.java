9890f1e64e5e7d51d00342ee31ab8bcaec103522
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-19 21:04:35 +0530

    File Service method created. Store file and Create File.

--- src/main/java/lk/spm/learning/management/service/FileStorageService.java
+++ src/main/java/lk/spm/learning/management/service/FileStorageService.java
@@ -1,9 +1,43 @@
 package lk.spm.learning.management.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileStorageService {
 
+    private Path fileStoragePath;
+    private String fileStorageLocation;
+
+    public FileStorageService (@Value("${file.storage.location:temp") String fileStorageLocation){
+        this.fileStorageLocation = fileStorageLocation;
+        fileStoragePath = Paths.get(fileStorageLocation).toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(fileStoragePath);
+        } catch (IOException e){
+            throw new RuntimeException("File creation Issue");
+        }
+    }
+
+    public String storeFile (MultipartFile file){
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path filePath = Paths.get(fileStoragePath + "\\" + fileName);
+
+        try {
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e){
+            throw new RuntimeException("Storing file error");
+        }
+        return fileName;
+    }
 }
