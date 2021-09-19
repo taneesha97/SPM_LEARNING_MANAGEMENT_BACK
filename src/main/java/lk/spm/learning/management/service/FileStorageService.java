c9569dbc422ae112dc8ef9d54377cf95da73a5a6
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-19 21:07:58 +0530

    Download file service method fixed.

--- src/main/java/lk/spm/learning/management/service/FileStorageService.java
+++ src/main/java/lk/spm/learning/management/service/FileStorageService.java
@@ -1,12 +1,15 @@
 package lk.spm.learning.management.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import org.springframework.core.io.Resource;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,5 +42,20 @@ public class FileStorageService {
             throw new RuntimeException("Storing file error");
         }
         return fileName;
+    }
+
+    public Resource downloadFile(String fileName){
+        Path path = Paths.get(fileStorageLocation).toAbsolutePath().resolve(fileName);
+        Resource resource;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch(MalformedURLException e){
+            throw new RuntimeException("Reading File Error");
+        }
+        if(resource.exists() && resource.isReadable()){
+            return resource;
+        } else {
+            throw new RuntimeException("File not available");
+        }
     }
 }
