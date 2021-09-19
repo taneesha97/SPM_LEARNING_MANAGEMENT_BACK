74884f9335b3de0da5f77c849d5aa8c19c1af362
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-19 15:25:07 +0530

    Model class created for the File

---
+++ src/main/java/lk/spm/learning/management/service/FileStorageService.java
@@ -0,0 +1,9 @@
+package lk.spm.learning.management.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileStorageService {
+
+}
