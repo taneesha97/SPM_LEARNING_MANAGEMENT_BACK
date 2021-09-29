08fbf7943a7e8c0fcc093e9cc122e6fb7cfd7199
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-29 11:39:11 +0530

    Lecture Videos Model Added.

---
+++ src/main/java/lk/spm/learning/management/model/LectureVideos.java
@@ -0,0 +1,52 @@
+package lk.spm.learning.management.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vidoes")
+public class LectureVideos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String name;
+
+    @Column(name = "title")
+    private String url;
+
+    public LectureVideos(){
+
+    }
+
+    public LectureVideos(long id, String name, String url) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
