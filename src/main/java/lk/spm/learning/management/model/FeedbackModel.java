7966361aa328609373ea73b6c5d2f4c0375bf073
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-29 11:25:06 +0530

    Feedback Model Added to the Project.

---
+++ src/main/java/lk/spm/learning/management/model/FeedbackModel.java
@@ -0,0 +1,39 @@
+package lk.spm.learning.management.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class FeedbackModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "message")
+    private String message;
+
+    public FeedbackModel(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
