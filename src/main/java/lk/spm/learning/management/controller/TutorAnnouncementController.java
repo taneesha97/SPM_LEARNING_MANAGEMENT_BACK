072b455f4bd978bc0e0100083acf77fcd37bac4a
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 20:00:55 +0530

    Tutor Announcement classes/Interfaces refactored.

--- src/main/java/lk/spm/learning/management/controller/TutorAnnouncementController.java
+++ src/main/java/lk/spm/learning/management/controller/TutorAnnouncementController.java
@@ -1,24 +1,21 @@
 package lk.spm.learning.management.controller;
 
 import lk.spm.learning.management.model.AnnouncementModel;
-import lk.spm.learning.management.model.Class;
-import lk.spm.learning.management.model.Course;
-import lk.spm.learning.management.repository.AnnouncementRepository;
+import lk.spm.learning.management.repository.TutorAnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
-public class AnnouncementController {
+public class TutorAnnouncementController {
 
     @Autowired
-    AnnouncementRepository announcementRepository;
+    TutorAnnouncementRepository announcementRepository;
 
     //GET ANNOUNCEMENTS
     @GetMapping("/announcements")
--- src/main/java/lk/spm/learning/management/repository/TutorAnnouncementRepository.java
+++ src/main/java/lk/spm/learning/management/repository/TutorAnnouncementRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<AnnouncementModel, String> {
+public interface TutorAnnouncementRepository extends JpaRepository<AnnouncementModel, String> {
 }
