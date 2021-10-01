5e03f3ec2297b7bb94fdd3b30ff772d4facbe76d
Taneesha <taneesha.chandrasena@acentura.com>
2021-10-01 18:39:51 +0530

    report generation partially done

--- src/main/java/lk/spm/learning/management/controller/ImageController.java
+++ src/main/java/lk/spm/learning/management/controller/ImageController.java
@@ -2,19 +2,25 @@ package lk.spm.learning.management.controller;
 
 import lk.spm.learning.management.model.FileModel;
 import lk.spm.learning.management.model.ImageModel;
+import lk.spm.learning.management.model.User;
+import lk.spm.learning.management.repository.Impl.UserImplementation;
+import lk.spm.learning.management.repository.loginUserRepository;
 import lk.spm.learning.management.service.FileStorageService;
 import lk.spm.learning.management.service.ImageStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -23,6 +29,11 @@ import java.util.List;
 public class ImageController {
 
     private final ImageStorageService imageStorageService;
+    @Autowired
+    private loginUserRepository loginUserRepository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public ImageController(ImageStorageService imageStorageService) {
         this.imageStorageService = imageStorageService;
@@ -65,5 +76,19 @@ public class ImageController {
         } else {
             return new ResponseEntity<>("No Files Available", HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/teachercountbyclass")
+    public ResponseEntity<?> getTeacherCountByClass(){
+
+        List<ImageModel> imageModels = loginUserRepository.getTutorListFromClasses();
+        try {
+            return new ResponseEntity<>(imageModels , HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage() , HttpStatus.OK);
+        }
+//        ArrayList users = new ArrayList();
+//        users.add(imageModels.size());
+//        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
